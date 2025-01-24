ce8955d86dc10beaf8cda3b1f3fe9b77de8abe9b
Kunal-Khare <kunalkhare47@gmail.com>
2025-01-24 19:33:56 +0530

    minor changes

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,32 +1,35 @@
 // this component is responsible for rendering the form to add a new todo item
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+function TodoForm({ addTodo }) {
+  const [input, setInput] = useState("");
 
-function TodoForm({addTodo}) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      addTodo(input);
+      setInput("");
+    }
+  };
 
-    const [input, setInput] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (input.trim()){
-            addTodo(input);
-            setInput('');
-        }
-    };
-
-    return(
-        <form onSubmit={handleSubmit} className='todo-form'>
-            <input type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder='Add a todo.'
-            className='border-2 p-2 mr-4 rounded' />
-            <button className='bg-blue-500 text-white px-4 py-2 rounded-2xl placeholder-color' style={{cursor: 'pointer'}}>Add</button>
-        </form>
-
-    )
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a todo."
+        className="border-2 p-2 mr-4 rounded"
+      />
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-2xl placeholder-color"
+        style={{ cursor: "pointer" }}
+      >
+        Add
+      </button>
+    </form>
+  );
 }
-
 
 export default TodoForm;
